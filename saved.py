41d40642c57bfcf58eeef8cba927e10a674e3df3
nmford2024 <nate.ford@go.sfcollege.edu>
2024-09-03 18:48:47 -0400

    Add files via upload

---
+++ saved.py
@@ -0,0 +1,7 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Creating a project for GitHub
+>>> #By James Nichols
+>>> 
+>>> print("COP2002 Fall 2030")
+COP2002 Fall 2030
